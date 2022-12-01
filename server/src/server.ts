50a4057b3f903b17381f28fb925019bcd247591e
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-12-01 15:31:43 -0300

    feat(server): create pools

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,8 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
+
+import ShortUniqueId from "short-unique-id";
+
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient({
@@ -18,6 +21,34 @@ async function start() {
   fastify.get("/pools/count", async () => {
     const count = await prisma.pool.count();
     return { count };
+  });
+
+  fastify.post("/pools", async (request, reply) => {
+    const { title }: any = request.body;
+
+    if (!title) {
+      return reply.status(400).send({
+        statusCode: 400,
+        error: "Missing param: title",
+      });
+    }
+
+    const generateId = new ShortUniqueId({ length: 6 });
+    const code = String(generateId()).toUpperCase();
+    try {
+      await prisma.pool.create({
+        data: {
+          title,
+          code,
+        },
+      });
+    } catch (error) {
+      return reply
+        .status(500)
+        .send({ statusCode: 500, error: "Internal server error" });
+    }
+
+    return reply.status(201).send({ statusCode: 201, code });
   });
 
   await fastify.listen({ port: 3333 });
