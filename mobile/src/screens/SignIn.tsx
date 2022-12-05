b07786ac8a0a990d5ab5d6302331396614c75d47
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-12-05 14:24:09 -0300

    chore(mobile): add react-svg-transformer

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,9 +1,10 @@
 import { Center, Heading, Text } from "native-base";
+import Logo from "../assets/logo.svg";
 
 export function SignIn() {
   return (
     <Center flex={1} bgColor="gray.800">
-      <Heading color="gray.100">SignIn</Heading>
+      <Logo />
     </Center>
   );
 }
