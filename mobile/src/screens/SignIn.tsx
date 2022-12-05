9ea04a32e70040ac12e75cedb5813a2a9578cbf8
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-12-05 14:48:26 -0300

    feat(mobile): create Button component

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,10 +1,19 @@
-import { Center, Heading, Text } from "native-base";
+import { Center, Heading, Text, Icon } from "native-base";
+import { Fontisto } from "@expo/vector-icons";
+
+import { Button } from "../components/Button";
+
 import Logo from "../assets/logo.svg";
 
 export function SignIn() {
   return (
     <Center flex={1} bgColor="gray.800">
-      <Logo />
+      <Logo width={212} height={40} />
+      <Button
+        title="ENTRAR COM O GOOGLE"
+        type="SECONDARY"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+      />
     </Center>
   );
 }
